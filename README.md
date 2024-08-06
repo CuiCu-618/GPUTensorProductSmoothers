e7d39d640ecf3c85bda6982266892804879f2867
CuiCu-618 <cuic3@mail2.sysu.edu.cn>
2024-08-06 12:57:58 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GPUTensorProductSmoothers
 A GPU implementation of vertex-patch smoothers for higher order finite element methods in two and three dimensions.
+
+## Getting started
+
+```bash
+git clone git@github.com:CuiCu-618/GPUTensorProductSmoothers.git
+cd GPUTensorProductSmoothers/
+mkdir build
+cd build/
+cp ../scripts/gputps-setup.sh .
+bash ./gputps-setup.sh
+make release
+make heat
+./apps/heat
+```
--- scripts/run_exec.sh
+++ scripts/run_exec.sh
@@ -1,24 +1,13 @@
 #!/bin/bash                                                                                                                           
 
-cmake .
 make release
 
-# for p in {1..10}
-# do
-#     echo "/////////////////////"
-#     echo "Starting 2D degree $p"
-#     echo "/////////////////////"
-#     python3 scripts/ct_parameter.py -DIM 2 -DEG $p -MAXSIZE 200000000 -REDUCE 1e-8
-#     make poisson
-#     ./apps/poisson
-# done
-
-for p in {2..8}
+for p in 2
 do
     echo "/////////////////////"
     echo "Starting 3D degree $p"
     echo "/////////////////////"
-    python3 scripts/ct_parameter.py -DIM 3 -DEG $p -MAXSIZE 5000000000
-    make poisson
-    ./apps/poisson
+    python3 scripts/ct_parameter.py -DIM 3 -DEG $p -MAXSIZE 500000
+    make heat  
+    ./apps/heat
 done
