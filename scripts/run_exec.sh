19553a8e036c03f94c4635dab8adcc1caa8244b7
CuiCu-618 <cuic3@mail2.sysu.edu.cn>
2024-08-19 20:47:37 +0200

    update script

--- scripts/run_exec.sh
+++ scripts/run_exec.sh
@@ -7,7 +7,7 @@ do
     echo "/////////////////////"
     echo "Starting 3D degree $p"
     echo "/////////////////////"
-    python3 scripts/ct_parameter.py -DIM 3 -DEG $p -N_STAGES 4 -DT 0.1 -ENDT 0.5 -REDUCE 1e-9 -REDUCE_INNER 1e-6 -K FUSED_L -MAXSIZE 5000000 -VNUM double
+    python3 scripts/ct_parameter.py -DIM 3 -DEG $p -N_STAGES 4 -DT 0.1 -ENDT 0.2 -REDUCE 1e-9 -REDUCE_INNER 1e-6 -K FUSED_L -MAXSIZE 5000000 -VNUM double -GNUM double
     make heat  
     ./apps/heat
 done
