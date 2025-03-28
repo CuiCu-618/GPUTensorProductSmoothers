22ddd005d6ecbbbad55883157b9a49f1d2bedeb6
CuiCu-618 <cuic3@mail2.sysu.edu.cn>
2025-03-28 23:18:45 +0100

    add cuda mpi tests

--- tests/test_mpi.cu
+++ tests/test_mpi.cu
@@ -32,8 +32,35 @@
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
 
+#include <nvToolsExt.h>
+
 #include <fstream>
 #include <iostream>
+
+const uint32_t colors[]   = {0x0000ff00,
+                             0x000000ff,
+                             0x00ffff00,
+                             0x00ff00ff,
+                             0x0000ffff,
+                             0x00ff0000,
+                             0x00ffffff};
+const int      num_colors = sizeof(colors) / sizeof(uint32_t);
+
+#define PUSH_RANGE(name, cid)                                          \
+  {                                                                    \
+    int color_id                      = cid;                           \
+    color_id                          = color_id % num_colors;         \
+    nvtxEventAttributes_t eventAttrib = {0};                           \
+    eventAttrib.version               = NVTX_VERSION;                  \
+    eventAttrib.size                  = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
+    eventAttrib.colorType             = NVTX_COLOR_ARGB;               \
+    eventAttrib.color                 = colors[color_id];              \
+    eventAttrib.messageType           = NVTX_MESSAGE_TYPE_ASCII;       \
+    eventAttrib.message.ascii         = name;                          \
+    nvtxRangePushEx(&eventAttrib);                                     \
+  }
+#define POP_RANGE nvtxRangePop();
+
 
 using namespace dealii;
 
@@ -61,47 +88,42 @@ main(int argc, char *argv[])
         AssertCuda(cuda_error_code);
       }
 
-      MPI_Comm           mpi_communicator(MPI_COMM_WORLD);
-      ConditionalOStream pcout(
-        std::cout, (Utilities::MPI::this_mpi_process(mpi_communicator) == 0));
 
-      pcout << "Running tests on "
-            << Utilities::MPI::n_mpi_processes(mpi_communicator)
-            << " MPI rank(s)..." << std::endl;
+      int world_rank;
+      MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-      LinearAlgebra::distributed::Vector<double, MemorySpace::CUDA> gpu_vec;
+      int         send_data, recv_data;
+      MPI_Request request[2];
+      MPI_Status  status[2];
 
-      IndexSet s(20);
-      IndexSet g(20);
-      if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
+      if (world_rank == 0)
         {
-          s.add_range(0, 10);
-          g.add_index(11);
+          send_data = 123;
+
+          MPI_Isend(&send_data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request[0]);
+
+          MPI_Irecv(&recv_data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request[1]);
+
+          printf("Rank 0 is doing computation...\n");
+
+          MPI_Waitall(2, request, status);
+
+          printf("Rank 0 received data from Rank 1: %d\n", recv_data);
         }
-      else
+      else if (world_rank == 1)
         {
-          s.add_range(10, 20);
-          g.add_index(9);
+          send_data = 456;
+
+          MPI_Isend(&send_data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request[0]);
+
+          MPI_Irecv(&recv_data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request[1]);
+
+          printf("Rank 1 is doing computation...\n");
+
+          MPI_Waitall(2, request, status);
+
+          printf("Rank 1 received data from Rank 0: %d\n", recv_data);
         }
-
-      gpu_vec.reinit(s, g, mpi_communicator);
-
-      gpu_vec = 1;
-      std::cout << "Init: \n";
-      gpu_vec.print(std::cout);
-
-      if (Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
-        write_ghost<<<1, 1>>>(gpu_vec.get_values(), 10);
-
-      cudaDeviceSynchronize();
-
-      std::cout << "before: \n";
-      gpu_vec.print(std::cout);
-
-      gpu_vec.compress(VectorOperation::add);
-
-      std::cout << "after: \n";
-      gpu_vec.print(std::cout);
     }
   catch (std::exception &exc)
     {
